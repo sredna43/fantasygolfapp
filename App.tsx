ad0ae62748b66d752756821a395b892ffff43991
Anders Olson <sredna43@gmail.com>
2023-04-08 16:33:02 -0500

    Get ready for real build

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
+// Root wrappers
 import { Provider as PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
+import { PreferencesContext } from "./PreferencesContext";
 
+// Functions
 import { useCallback, useMemo, useState } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { PreferencesContext } from "./PreferencesContext";
+
+// Styles
 import { CombinedDefaultTheme, CombinedDarkTheme } from "./config/CustomTheme";
+
+// Components
 import CustomNavBar from "./components/CustomNavBar";
+
+// Pages
 import HomeScreen from "./pages/HomeScreen";
-import DraftScreen from "./pages/DraftScreen";
 import LeagueHomeScreen from "./pages/LeagueHomeScreen";
 
 const Stack = createNativeStackNavigator();
@@ -37,7 +44,6 @@ export default function App() {
             screenOptions={{ header: (props) => <CustomNavBar {...props} /> }}
           >
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Draft" component={DraftScreen} />
             <Stack.Screen name="LeagueHome" component={LeagueHomeScreen} />
           </Stack.Navigator>
         </NavigationContainer>
--- service/leagueService.ts
+++ service/leagueService.ts
@@ -1,3 +1,4 @@
+import { Golfer } from "../types/Golfer";
 import { apiConfig, env } from "./apiConfig";
 
 export function createLeague(leagueName: string, isPrivate: boolean) {
@@ -42,6 +43,67 @@ export function seachForLeagues(leagueName: string) {
   const url = `${apiConfig[env].baseUrl}/league/search?${params}`;
   return fetch(url, {
     method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export function addGolferToLeague(golfer: Golfer, leagueId: string) {
+  const url = `${apiConfig[env].baseUrl}/league/golfer`;
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      league_id: leagueId,
+    },
+    body: JSON.stringify(golfer),
+  });
+}
+
+export function editGolferInLeague(golfer: Golfer, leagueId: string) {
+  const url = `${apiConfig[env].baseUrl}/league/golfer`;
+  return fetch(url, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      league_id: leagueId,
+    },
+    body: JSON.stringify(golfer),
+  });
+}
+
+export function removeGolferFromLeague(golfer: Golfer, leagueId: string) {
+  const url = `${apiConfig[env].baseUrl}/league/golfer`;
+  return fetch(url, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      league_id: leagueId,
+    },
+    body: JSON.stringify(golfer),
+  });
+}
+
+export function addPlayerToLeague(userId: string) {
+  const url = `${apiConfig[env].baseUrl}/league/player/add/${userId}`;
+  return fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export function removePlayerFromLeague(userId: string) {
+  const url = `${apiConfig[env].baseUrl}/league/player/remove/${userId}`;
+  return fetch(url, {
+    method: "DELETE",
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
